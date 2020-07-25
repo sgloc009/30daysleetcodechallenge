fe82733074fea4566ddae5841034617a84ee0469
sgloc009 <rickirod007@gmail.com>
2020-07-25 22:16:04 +0530

    Create find_min_in_rotated_sorted_array_2.py

---
+++ find_min_in_rotated_sorted_array_2.py
@@ -0,0 +1,20 @@
+#July_25th 2020 Leetcode challenge
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        min = 0
+        return(self.recursiveBS(nums))
+        
+    def recursiveBS(self, nums):
+        if(len(nums)==1):
+            return(nums[0])
+        pivot = len(nums)//2
+        lhs = self.recursiveBS(nums[:pivot])
+        print(lhs)
+        rhs = self.recursiveBS(nums[pivot:])
+        print(rhs)
+        if(lhs>nums[pivot] and nums[pivot]<rhs):
+            return nums[pivot]
+        elif(lhs>rhs):
+            return rhs
+        else:
+            return lhs
